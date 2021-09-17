cc578af1760df94a39d4e4c60ce0a9f9d92552ea
assahasranamam <siradithyass@gmail.com>
2021-09-17 03:27:36 -0400

    productionEnvUpdate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+
+    apiKey: "AIzaSyD8QKDrELdINAMkaoIaIUAATIrt18XCMkQ",
+
+    authDomain: "adi-personal-test.firebaseapp.com",
+
+    databaseURL: "https://adi-personal-test.firebaseio.com",
+
+    projectId: "adi-personal-test",
+
+    storageBucket: "adi-personal-test.appspot.com",
+
+    messagingSenderId: "994495293252",
+
+    appId: "1:994495293252:web:3d4abae012637f79fc1911"
+
+  }
+
 };
